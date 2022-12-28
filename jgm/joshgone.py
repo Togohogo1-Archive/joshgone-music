1aad12a9cb09fc402cffb9f3cf7715b3e77654be
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2022-12-28 00:01:12 -0500

    removed unneded extensions

--- jgm/joshgone.py
+++ jgm/joshgone.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 # These extensions are loaded automatically on startup
 LOAD_ON_STARTUP = (
-    "admin", "chant", "music", "database", "gee", "remind", "split", "get", "sort", "info"
+    "admin", "chant", "music", "database", "info"
 )
 
 # We need intents to resolve a name to a Member object
