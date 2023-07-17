7df5c059d8464eaacf6f0073a9e6705cf93aded5
Kevin Wang <59260907+Togohogo1@users.noreply.github.com>
2023-07-17 00:01:21 -0400

    Chant 5 times with timeout no need -> change to 1

--- jgm/extensions/chant.py
+++ jgm/extensions/chant.py
@@ -319,37 +319,25 @@ class Chant(commands.Cog):
             except Exception as e:
                 print(f'Error notifying cron cog: {e!r}')
 
-    @commands.command(name="chant", aliases=["h"], ignore_extra=False)
-    async def _chant(self, ctx, name: str, repeats: int = 5, delay: float = 2):
+    @commands.command(name="chant", aliases=["h1"], ignore_extra=False)
+    async def _chant(self, ctx, name: str):
         """Repeat a chant multiple times
 
         `repeat` specifies the number of times to repeat the chant
         `delay` specifies the number of seconds to wait between chants
         """
-        if not math.isfinite(delay):
-            raise ValueError(f"{delay!r} is not finite")
-        for _ in range(repeats):
-            async with aiosqlite.connect(os.environ["JOSHGONE_DB"]) as db:
-                async with db.execute("SELECT running FROM server WHERE server_id = ? LIMIT 1;", (ctx.guild.id,)) as cursor:
-                    if not (row := await cursor.fetchone()) or not row[0]:
-                        break
-                async with db.execute("SELECT chant_text FROM chants WHERE server_id = ? AND chant_name = ? LIMIT 1;", (ctx.guild.id, name)) as cursor:
-                    row = await cursor.fetchone()
-                    if not row:
-                        break
-                    text = row[0]
-                    if not text:
-                        break
-            await ctx.send(text)
-            await asyncio.sleep(delay)
-
-    @commands.command(name="chant1", aliases=["h1"], ignore_extra=False)
-    async def _chant1(self, ctx, name: str, delay: float = 2):
-        """Repeats a chant once
-
-        `delay` specifies the number of seconds to wait between chants
-        """
-        await self._chant(ctx, name, 1, delay)
+        async with aiosqlite.connect(os.environ["JOSHGONE_DB"]) as db:
+            async with db.execute("SELECT running FROM server WHERE server_id = ? LIMIT 1;", (ctx.guild.id,)) as cursor:
+                if not (row := await cursor.fetchone()) or not row[0]:
+                    return
+            async with db.execute("SELECT chant_text FROM chants WHERE server_id = ? AND chant_name = ? LIMIT 1;", (ctx.guild.id, name)) as cursor:
+                row = await cursor.fetchone()
+                if not row:
+                    return
+                text = row[0]
+                if not text:
+                    return
+        await ctx.send(text)
 
 def setup(bot):
     return bot.add_cog(Chant(bot))
